590a2d92185153662fb388daf47df0dfa72a1694
Timo Breumelhof <timo@40fingers.net>
2026-03-17 16:54:06 +0100

    move build to build folder

---
+++ _build/tasks/watch.js
@@ -0,0 +1,23 @@
+/**
+ * Watch tasks
+ */
+
+const { watch, series } = require('gulp');
+const { buildSkinsToDist, buildContainersToDist, buildScss, buildJs, copyVendors } = require('./build');
+const { cleanSkins, cleanContainers } = require('./clean');
+const { distributeSkins, distributeContainers } = require('./distribute');
+
+/**
+ * Watch files for changes
+ */
+function watchFiles() {
+  watch('skin/**/*', series(buildSkinsToDist, cleanSkins, distributeSkins));
+  watch('container/**/*', series(buildContainersToDist, cleanContainers, distributeContainers));
+  watch('src/scss/**/*.scss', series(buildScss, cleanSkins, distributeSkins));
+  watch('src/js/**/*.js', series(buildJs, cleanSkins, distributeSkins));
+  watch('vendors/**/*', series(copyVendors, cleanSkins, distributeSkins));
+}
+
+module.exports = {
+  watchFiles,
+};
